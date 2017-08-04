968d65c16981d650f4b661793de1e6c750bb8477
Konstantin Makhov <30411716+kmakhov@users.noreply.github.com>
2017-08-04 16:17:33 +0300

    Create mario1.c

---
+++ Week1/mario1.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) 
+{
+	int height = 0;
+	do 
+	{
+		printf("Height:");
+		height = get_int();
+        if (height <= 0 || height >=23 )
+        {
+        	printf("error\n");
+        }
+	}
+	while (height < 1 || height > 23);
+	for(int row = 0; row < height; row++) 
+	{S
+        for(int space = 0; space < height-row-1; space++)
+        {
+            printf(" ");
+        }
+		for(int hashtag = 0; hashtag < row+1; hashtag++)
+		{
+			printf("#");
+		}
+		printf("  ");
+		for(int hashtag = 0; hashtag < row+1; hashtag++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}	
+}
